8c9cf253223617304109153021a3f586ead9916e
techiphyte76 <77113884+techiphyte76@users.noreply.github.com>
2021-02-12 15:34:40 -0500

    added way to account for on called for dates

--- app.js
+++ app.js
@@ -2,44 +2,68 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const token = "ODA5NzkxMzU1MzcwNDcxNDg2.YCaO-A.NnJEz6kR0-FEw3BYHRela1RX4bs";
 const cron = require("cron");
-const app = require("express")
-const http = require('http')
-const fs = require('fs')
+const app = require("express");
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('public/index.html').pipe(res)
-})
+  res.writeHead(200, { "content-type": "text/html" });
+  fs.createReadStream("public/index.html").pipe(res);
+});
 
-server.listen(process.env.PORT || 3000)
+server.listen(process.env.PORT || 3000);
 
-const now = new Date; 
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const now = new Date();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-let arDates = new Map(); 
-
+let arDates = new Map();
 arDates = [
-    {date: "1_12", arPeriod: "B 6/7", rotationDay: "C"},
-    {date: "1_16", arPeriod: "C 6/7", rotationDay: "A"},
-    {date: "1_17", arPeriod: "A 1/2", rotationDay: "B"},
-    {date: "1_18", arPeriod: "B 1/2", rotationDay: "C"},
-    {date: "1_19", arPeriod: "C 1/2", rotationDay: "A"},
-    {date: "1_20", arPeriod: "A 4/5", rotationDay: "B"},
-]
+  { date: "1_16", arPeriod: "C 6/7", rotationDay: "A" },
+  { date: "1_17", arPeriod: "A 1/2", rotationDay: "B" },
+  { date: "1_18", arPeriod: "B 1/2", rotationDay: "C" },
+  { date: "1_19", arPeriod: "C 1/2", rotationDay: "A" },
+  { date: "1_20", arPeriod: "A 4/5", rotationDay: "B" },
+];
+
+let currentInfo = arDates.find(
+  (arDates) => arDates.date === `${now.getMonth()}_${now.getDate()}`
+);
+
 
 bot.login(token);
 
 bot.on("ready", () => {
   console.log(`The Bot is logged in as ${bot.user.tag}.`);
-  console.log(`${now.getMonth()}_${now.getDate()}`)
-  let all = arDates.find(arDates => arDates.date===`${now.getMonth()}_${now.getDate()}`)
-  console.log(all)
+  console.log(`${now.getMonth()}_${now.getDate()}`);
+  console.log(currentInfo);
 }); // You don't need to add anything to the message event listener
 
 bot.on("message", (msg) => {
   if (msg.content === "!ar") {
-    let currentInfo = arDates.find(arDates => arDates.date===`${now.getMonth()}_${now.getDate()}`)
-    msg.reply(`I am AR Bot and the day is ${months[now.getMonth()]} ${now.getDate()}th.`);
-    msg.reply(`The rotation day is : ${currentInfo.rotationDay}. The AR period is: ${currentInfo.arPeriod}.`)
-  } 
+    if (currentInfo !== undefined) {
+      msg.reply(
+        `I am AR Bot and the day is ${
+          months[now.getMonth()]
+        } ${now.getDate()}th.`
+      );
+      msg.reply(
+        `The rotation day is : ${currentInfo.rotationDay}. The AR period is: ${currentInfo.arPeriod}.`
+      );
+    } else {
+      msg.reply("The date is not valid")
+    }
+  }
 });
