a5c993125a272f887512aa7e609845d29ae6243f
techiphyte76 <77113884+techiphyte76@users.noreply.github.com>
2021-03-05 16:36:20 -0500

    Added NASA back

---
+++ commands/nasa.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+
+module.exports = {
+  name: "nasa",
+  description: "Fetches NASA's Astronomy Image of the Day",
+  async execute(message) {
+    let getNasa = async () => {
+      let response = await axios.get(
+        "https://api.nasa.gov/planetary/apod?api_key=0MASGs4gcTRGjh3nEcBQ22qq3pNlGvQQHXH4Mzy8"
+      );
+      let nasa = response.data.hdurl;
+      return nasa;
+    };
+
+    let nasaValue = await getNasa();
+    msg.channel.send({ files: [nasaValue] });
+  },
+};
