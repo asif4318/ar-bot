4be30a5404ac3b5713e567e52ab26304ad749f2a
techiphyte76 <77113884+techiphyte76@users.noreply.github.com>
2021-04-09 09:25:23 -0400

    Added cron job to automate an AR message at 9:00 am on weekdays

--- app.js
+++ app.js
@@ -1,53 +1,92 @@
-
-const fs = require('fs');
-const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const fs = require("fs");
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
+const { ar } = require("./fetchAR.js");
+const cron = require("cron");
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
+client.once("ready", () => {
+  console.log("Ready!");
+  const fetchGuilds = () => client.guilds.cache.map((g) => g); //Obtains array of all guilds (servers)
+  // Fetches list of all system Channels
+  const fetchSystemChannels = () => {
+    systemChannels = [];
+    for (i = 0; i < fetchGuilds().length; i++) {
+      if (fetchGuilds()[i].systemChannelID !== null) {
+        systemChannels.push(fetchGuilds()[i].systemChannelID);
+      } else if (fetchGuilds()[i].name === 'general') {
+		systemChannels.push(fetchGuilds()[i].systemChannelID);
+	  }
+    }
+    return systemChannels;
+  };
+  console.log(fetchSystemChannels());
+  //Messages all systemChannels
+  /*for (i = 0; i < systemChannels.length; i++) {
+    client.channels.cache
+      .get(systemChannels[i])
+      .send(
+        "If you are interested in receiving automatic AR reminders at 9:00am on weekdays, please create a #ar-bot channel!"
+      );
+  }*/
 
-client.once('ready', () => {
-	console.log('Ready!');
-	const guildIDs = client.guilds.cache.map(g => g); //Obtains array of all guild IDs (servers)
-	// Finds all channels with name 'ar-bot' and get ID
-	let allChannels = client.channels.cache.map(c => c);
-	let arBotChannels = [];
-	for (i=0; i < allChannels.length; i++) {
-		if (allChannels[i].name === 'ar-bot') {
-			arBotChannels.push(allChannels[i].id)
-		}
-	}
-	//console.log(annoucementsChannel.id);
-	console.log(arBotChannels);
-	for (i=0; i < arBotChannels.length; i++) {
-		client.channels.cache.get(arBotChannels[i]).send('Hello here!')
-	}
-	//test = client.commands.get('ar').execute(null, null);
-	//console.log(test);
+  // Finds all channels with name 'ar-bot' and get ID
+  const fetchArBotChannels = () => {
+    const allChannels = () => client.channels.cache.map((c) => c);
+    let arBotChannels = [];
+    for (i = 0; i < allChannels().length; i++) {
+      if (allChannels()[i].name === "ar-bot") {
+        arBotChannels.push(allChannels()[i].id);
+      }
+    }
+    return arBotChannels;
+  };
+
+  //Schedule cron job
+  let arJob = new cron.CronJob(
+    "0 0 9 * * 1-5",
+    function () {
+      for (i = 0; i < fetchArBotChannels().length; i++) {
+        try {
+          client.channels.cache.get(fetchArBotChannels()[i]).send(ar()[0]);
+          client.channels.cache.get(fetchArBotChannels()[i]).send(ar()[1]);
+        } catch (err) {
+          console.log("Error");
+        }
+      }
+    },
+    null,
+    true,
+    "America/New_York"
+  );
+  arJob.start();
 });
 
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
 
-	if (!client.commands.has(command)) return;
+  if (!client.commands.has(command)) return;
 
-	try {
-		client.commands.get(command).execute(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('There was an error trying to execute that command!');
-	}
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("There was an error trying to execute that command!");
+  }
 });
 
 client.login(token);
---
+++ fetchAR.js
@@ -0,0 +1,119 @@
+const {
+  arOffset,
+  startOfSemester,
+  endOfSemester,
+  semesterHolidays,
+  rotationDayOffset,
+} = require("../config.json");
+
+generateSchoolDaysForSemester = {
+  arRotation: [
+    "A 1/2",
+    "B 1/2",
+    "C 1/2",
+    "A 4/5",
+    "B 4/5",
+    "C 4/5",
+    "A 6/7",
+    "B 6/7",
+    "C 6/7",
+  ],
+  rotationDays: ["A", "B", "C"],
+  allSchoolDays: function (start, end, blockOutDates) {
+    for (
+      var arr = [], dt = new Date(start);
+      dt <= end;
+      dt.setDate(dt.getDate() + 1)
+    ) {
+      if (
+        ![0, 6].includes(dt.getDay()) &&
+        !blockOutDates.includes(
+          dt.getMonth() + "_" + dt.getDate() + "_" + dt.getFullYear()
+        )
+      ) {
+        arr.push(
+          new Date(dt).getMonth() +
+            "_" +
+            new Date(dt).getDate() +
+            "_" +
+            new Date(dt).getFullYear()
+        );
+      }
+    }
+    return arr;
+  },
+  appendSessionAR: function (start, end, blockOutDates) {
+    var values = [];
+    z = this.allSchoolDays(start, end, blockOutDates);
+    for (var i = 0; i < z.length; i++) {
+      values[i] = {
+        date: z[i],
+        ar: this.arRotation[(i + arOffset) % this.arRotation.length],
+        session: Math.floor(i / 3),
+        rotationDay: this.rotationDays[(i + rotationDayOffset) % 3],
+      };
+    }
+    return values;
+  },
+};
+
+function arDateReply(checkTomorrow) {
+  let semesterSchoolDays = generateSchoolDaysForSemester.appendSessionAR(
+    new Date(startOfSemester),
+    new Date(endOfSemester),
+    semesterHolidays
+  );
+  const timeNow = new Date();
+  if (checkTomorrow === true) {
+    timeNow.setDate(timeNow.getDate() + 1);
+    console.log("arDate reply tomorrow " + timeNow.getDate());
+  }
+  timeNowDayDate =
+    timeNow.getMonth() + "_" + timeNow.getDate() + "_" + timeNow.getFullYear();
+  infoNow = semesterSchoolDays.find((e) => e.date === timeNowDayDate);
+  replyContent = `The rotation day is: ${infoNow.rotationDay}. The session is ${infoNow.session}. The AR period is ${infoNow.ar}.`;
+  return replyContent;
+}
+
+function monthDateReply(checkTomorrow) {
+  const timeNow = new Date();
+  replyContent = `I am AR Bot and the date is:  ${timeNow.toLocaleDateString()}. The time is: ${timeNow.toLocaleTimeString()}.`;
+  if (checkTomorrow === true) {
+    timeNow.setDate(timeNow.getDate() + 1);
+    replyContent = `I am AR Bot and tomorrow's date is:  ${timeNow.toLocaleDateString()}. The time is: ${timeNow.toLocaleTimeString()}.`;
+  }
+  return replyContent;
+}
+
+function getCurrentDayDate(checkTomorrow) {
+  currentTime = new Date();
+  x = currentTime.getMonth();
+  y = currentTime.getDate();
+  let dayDate = `${x}_${y}`;
+  return dayDate;
+}
+
+module.exports = {
+  ar: function() {
+    const timeNow = new Date();
+    let isTomorrow = false;
+    timeNowDayDate =
+    timeNow.getMonth() + "_" + timeNow.getDate() + "_" + timeNow.getFullYear();
+    let z = generateSchoolDaysForSemester.appendSessionAR(
+      new Date(startOfSemester),
+      new Date(endOfSemester),
+      []
+    );
+    getDayInfo = z.find((z) => z.date === timeNowDayDate);
+    if (
+      timeNow.getDay() === 6 ||
+      timeNow.getDay() === 0 ||
+      getDayInfo == undefined
+    ) {
+      return ("There is no school today, please enjoy your weekend!");
+    } else if (getCurrentDayDate() !== undefined) {
+     return [arDateReply(isTomorrow), monthDateReply(isTomorrow)];
+    }
+  }
+}
+
