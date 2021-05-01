e5f3725883e029cdd47fd4bddd6fdcdf84ff43a3
techiphyte76 <77113884+techiphyte76@users.noreply.github.com>
2021-05-01 13:45:23 -0400

    Added past papers command

---
+++ commands/pastpapers.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "pastpapers",
+  description: "Returns past papers",
+  async execute(message) {
+    message.reply('The link to past papers is here: https://drive.google.com/drive/folders/1FQp9O5Czv4UQmxq7JsHkeR46gdGsZQCX?usp=sharing');
+  },
+};
