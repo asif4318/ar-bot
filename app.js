dcef74e5af5be0e64e895acf3e66e284730b27d2
techiphyte76 <77113884+techiphyte76@users.noreply.github.com>
2021-05-09 10:31:22 -0400

    Cleaned up code with prettier and extended range of duck command search results

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const fs = require("fs"); // Reads filesystem to see config.json
-const Discord = require("discord.js"); //Discord API wrapper
-const { prefix, token } = require("./config.json"); //Import the prefix (ex: *, !, $) from the config file
+const fs = require('fs'); // Reads filesystem to see config.json
+const Discord = require('discord.js'); //Discord API wrapper
+const { prefix, token } = require('./config.json'); //Import the prefix (ex: *, !, $) from the config file
 // and token required to authorize the bot
-const { ar } = require("./fetchAR.js"); //AR command to execute on cron job
-const cron = require("cron"); //Timing library to send AR message at specified time every weekday.
+const { ar } = require('./fetchAR.js'); //AR command to execute on cron job
+const cron = require('cron'); //Timing library to send AR message at specified time every weekday.
 
 //Initializing Disocrd Client
 const client = new Discord.Client();
@@ -12,39 +12,39 @@ client.commands = new Discord.Collection();
 
 //Read all files in directy commands that end with a .js extension
 const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
+    .readdirSync('./commands')
+    .filter((file) => file.endsWith('.js'));
 
 //Adds commands to collection
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
 }
 
 // Commands to be performed when the bot is initialized
-client.once("ready", () => {
-  console.log("Ready!");
+client.once('ready', () => {
+    console.log('Ready!');
 
-  //Sets listening to help status on Discord
-  client.user.setActivity(`Listening to ${prefix}help`);
+    //Sets listening to help status on Discord
+    client.user.setActivity(`Listening to ${prefix}help`);
 
-  const fetchGuilds = () => client.guilds.cache.map((g) => g); //Obtains array of all guilds (servers)
-  const fetchGuildsName = () => client.guilds.cache.map((g) => g.name);
-  
-  // Fetches list of all System Channels
-  const fetchSystemChannels = () => {
-    systemChannels = [];
-    for (i = 0; i < fetchGuilds().length; i++) {
-      if (fetchGuilds()[i].systemChannelID !== null) {
-        systemChannels.push(fetchGuilds()[i].systemChannelID);
-      } else if (fetchGuilds()[i].name === "general") {
-        systemChannels.push(fetchGuilds()[i].systemChannelID);
-      }
-    }
-    return systemChannels;
-  };
-  //Messages all systemChannels
-  /*for (i = 0; i < systemChannels.length; i++) {
+    const fetchGuilds = () => client.guilds.cache.map((g) => g); //Obtains array of all guilds (servers)
+    const fetchGuildsName = () => client.guilds.cache.map((g) => g.name);
+
+    // Fetches list of all System Channels
+    const fetchSystemChannels = () => {
+        systemChannels = [];
+        for (i = 0; i < fetchGuilds().length; i++) {
+            if (fetchGuilds()[i].systemChannelID !== null) {
+                systemChannels.push(fetchGuilds()[i].systemChannelID);
+            } else if (fetchGuilds()[i].name === 'general') {
+                systemChannels.push(fetchGuilds()[i].systemChannelID);
+            }
+        }
+        return systemChannels;
+    };
+    //Messages all systemChannels
+    /*for (i = 0; i < systemChannels.length; i++) {
     client.channels.cache
       .get(systemChannels[i])
       .send(
@@ -52,19 +52,19 @@ client.once("ready", () => {
       );
   }*/
 
-  // Finds all channels with name 'ar-bot' and map IDs into an array
-  const fetchArBotChannels = () => {
-    const allChannels = () => client.channels.cache.map((c) => c);
-    let arBotChannels = [];
-    for (i = 0; i < allChannels().length; i++) {
-      if (allChannels()[i].name === "ar-bot") {
-        arBotChannels.push(allChannels()[i]);
-      }
-    }
-    return arBotChannels;
-  };
+    // Finds all channels with name 'ar-bot' and map IDs into an array
+    const fetchArBotChannels = () => {
+        const allChannels = () => client.channels.cache.map((c) => c);
+        let arBotChannels = [];
+        for (i = 0; i < allChannels().length; i++) {
+            if (allChannels()[i].name === 'ar-bot') {
+                arBotChannels.push(allChannels()[i]);
+            }
+        }
+        return arBotChannels;
+    };
 
-  /*function testARChannels() {
+    /*function testARChannels() {
     for (const value of fetchArBotChannels()) {
       try {
         value.send(ar()[0]);
@@ -76,47 +76,47 @@ client.once("ready", () => {
     }
   }
 
-  testARChannels();*/ 
+  testARChannels();*/
 
-  //Schedule cron job to send an AR messsage at 9:00am every weekday
-  let arJob = new cron.CronJob(
-    "0 0 9 * * 1-5",
-    function () {
-      for (const channel of fetchArBotChannels()) {
-        try {
-          channel.send(ar()[0]);
-          channel.send(ar()[1]);
-          console.log('Sent AR message');
-        } catch (err) {
-          console.log(err);
-        }
-      }
-    },
-    null,
-    true,
-    "America/New_York"
-  );
-  arJob.start();
+    //Schedule cron job to send an AR messsage at 9:00am every weekday
+    let arJob = new cron.CronJob(
+        '0 0 9 * * 1-5',
+        function () {
+            for (const channel of fetchArBotChannels()) {
+                try {
+                    channel.send(ar()[0]);
+                    channel.send(ar()[1]);
+                    console.log('Sent AR message');
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+        },
+        null,
+        true,
+        'America/New_York'
+    );
+    arJob.start();
 });
 
 //Commands to be performed when a message is sent
-client.on("message", (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on('message', (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  //Defines arguments as content right after the prefix, splitting at spaces
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  //Shifts all arguments to lower case such that bot commands are not case sensitive
-  const command = args.shift().toLowerCase();
+    //Defines arguments as content right after the prefix, splitting at spaces
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    //Shifts all arguments to lower case such that bot commands are not case sensitive
+    const command = args.shift().toLowerCase();
 
-  //Checks if the command provided is part of the command collections and returns if false.
-  if (!client.commands.has(command)) return;
-  //Else if the command exists within the collection - execute
-  try {
-    client.commands.get(command).execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply("There was an error trying to execute that command!");
-  }
+    //Checks if the command provided is part of the command collections and returns if false.
+    if (!client.commands.has(command)) return;
+    //Else if the command exists within the collection - execute
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('There was an error trying to execute that command!');
+    }
 });
 
 client.login(token);
--- commands/duck.js
+++ commands/duck.js
@@ -1,19 +1,19 @@
-const axios = require("axios");
+const axios = require('axios');
 
 module.exports = {
-  name: "duck",
-  description: "Advanced duck search using Google",
-  async execute(message) {
-    apiLink =
-      "https://www.googleapis.com/customsearch/v1?key=AIzaSyDxRGE_aokhglI6ayaYw4hIk6x_ebkHROY&cx=ca71548ca44eba24f&q=ducks&searchType=image";
+    name: 'duck',
+    description: 'Advanced duck search using Google',
+    async execute(message) {
+        apiLink =
+            'https://www.googleapis.com/customsearch/v1?key=AIzaSyDxRGE_aokhglI6ayaYw4hIk6x_ebkHROY&cx=ca71548ca44eba24f&q=ducks&searchType=image';
 
-    random = Math.floor(Math.random() * 30);
-    random2 = Math.floor(Math.random() * 10);
-    if (random > 10) {
-      apiLink += `&start=${random}`;
-    }
-    response = await axios.get(apiLink);
-    let items = response.data.items;
-    message.channel.send(items[random2].link)
-  },
+        random = Math.floor(Math.random() * 90); //Chooses a starting index as an integer from 0 - 300
+        random2 = Math.floor(Math.random() * 10); //Google JSON API only returns ten results at a time
+        if (random > 10) {
+            apiLink += `&start=${random}`;
+        }
+        response = await axios.get(apiLink);
+        let items = response.data.items;
+        message.channel.send(items[random2].link);
+    },
 };
